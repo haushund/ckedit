cb163820dd5249386cfd57c098b2ab2690932000
Haushund <haushund98@gmail.com>
2015-04-15 21:30:09 +0200

    Start

---
+++ ckeditor/plugins/TS_DocBookSelecter/plugin.js
@@ -0,0 +1,17 @@
+﻿
+CKEDITOR.plugins.add( 'TS_DocBookSelecter', {
+    icons: 'Pirate',
+	init: function( editor ) {
+		editor.addCommand( 'selectFile', {
+            exec: function( editor ) {
+                
+            }
+        });
+        editor.ui.addButton( 'Pirate', {
+            label: 'DocBook',
+            command: 'selectFile',
+            toolbar: 'document'
+        });	
+    }
+});
+
---
+++ ckeditor/plugins/TS_ToDo/plugin.js
@@ -0,0 +1,14 @@
+﻿
+CKEDITOR.plugins.add( 'TS_ToDo', {
+    init: function( editor ) {
+		console.log ( "init_ToDo");
+		console.log ( CKEDITOR);
+		//console.log ( CKEDITOR.editor);
+		
+        //Plugin logic goes here.
+		//Resize the Editor
+		//Add a list on the right hand side of the editor
+		
+    }
+});
+
